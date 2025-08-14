5ae4b68f0ff33d50d342c084b1454ffc8ec0b4eb
Jijoo Maeng <mjiju06@gmail.com>
2025-08-15 00:22:36 +0900

    feat: swift 프로젝트 추가 및 ml모델 적용

---
+++ car_damage/car_damage/car_damageApp.swift
@@ -0,0 +1,17 @@
+//
+//  car_damageApp.swift
+//  car_damage
+//
+//  Created by JiJooMaeng on 8/12/25.
+//
+
+import SwiftUI
+
+@main
+struct car_damageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
